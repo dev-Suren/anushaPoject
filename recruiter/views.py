f66cbf3db60917fdf58671acad2d5fd91c024847
Suren <surenrana113@gmail.com>
2021-03-21 12:14:33 +0545

    just showing landing page

--- recruiter/views.py
+++ recruiter/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def recruiterHome(request):
-	return HttpResponse('THis is recruiter dashboard')
+	return render(request, 'landingpage.html')
 
